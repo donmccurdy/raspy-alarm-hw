bc2afca79d55c2c186644f5824c8f8fb4559d4f4
Don McCurdy <don.r.mccurdy@gmail.com>
2015-09-24 23:15:51 -0400

    Use branched version of J5 with HT16K33 digit support.

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ To get the `node-speaker` package working on a Pi, the ALSA package must be inst
 ```bash
 $ sudo apt-get install libasound2-dev
 ```
+
+## Notes
+
+If the Raspberry Pi is showing Universal rather than Local time, try `dpkg-reconfigure tzdata` as root. I also reinstalled NTPD, which may have been part of the fix.
--- src/clock/display.js
+++ src/clock/display.js
@@ -2,9 +2,6 @@ var _ = require('lodash'),
 	five = require('johnny-five'),
 	Raspi = require('raspi-io');
 
-var DIGITS = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F],
-	DEFAULTS = { brightess: 50 };
-
 /**
  * Display
  *
@@ -13,45 +10,27 @@ var DIGITS = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F],
  */
 function Display () {
 	this.board = new five.Board({io: new Raspi()});
-	this.matrix = null;
+	this.led = null;
 
 	this.board.on('ready', function () {
 		// Create HT16K33 controller.
-		this.matrix = new five.Led.Matrix({controller: 'HT16K33'});
-		this.matrix.brightness(DEFAULTS.brightness);
-
+		this.led = new five.Led.Digits({controller: 'HT16K33', digits: 5});
 		// Set time, if provided.
 		if (this.moment) this.set(this.moment);
 	}.bind(this));
-
 }
 
 _.merge(Display.prototype, {
 
 	set: function (moment) {
-		var hours = moment.format('hh'),
-			minutes = moment.format('mm');
-
 		this.moment = moment;
-
-		if (this.matrix) {
-			// Display time. This is a hack because the LEDControl assumes
-			// that all HT16K33 controllers are driving matrices.
-			// See: https://github.com/rwaldron/johnny-five/issues/905
-			this.matrix.displaybuffers[0][0] = DIGITS[hours[0]];
-			this.matrix.displaybuffers[0][1] = DIGITS[hours[1]];
-			this.matrix.displaybuffers[0][2] = 0xFFFF;
-			this.matrix.displaybuffers[0][3] = DIGITS[minutes[0]];
-			this.matrix.displaybuffers[0][4] = DIGITS[minutes[1]];
-
-			// Flush buffers.
-			this.matrix.writeDisplay(0);
+		if (this.led) {
+			this.led.print(moment.format('hh:mm'));
 		}
-
 	},
 
 	clear: function () {
-		this.matrix.clear();
+		this.led.clear();
 	}
 
 });
